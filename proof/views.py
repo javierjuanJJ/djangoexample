16fc6646c9658663d6f45ba344aaf32f5fab935a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-01-31 10:23:13 +0100

    Commit 6 Template Views

--- proof/views.py
+++ proof/views.py
@@ -5,16 +5,16 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>Hellow World</h1>")
+    return render(request, "home.html",{})
 
 
 def contacts_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contacts view</h1>")
+    return render(request, "contacts.html",{})
 
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About view</h1>")
+    return render(request, "about.html",{})
 
 
 def social_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Social view</h1>")
+    return render(request, "social.html", {})
