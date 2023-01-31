ebb20682eafbde43ae7d1b37a2e9734285a94f3d
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-01-31 10:35:22 +0100

    Commit 8 context

--- proof/views.py
+++ proof/views.py
@@ -13,7 +13,12 @@ def contacts_view(request, *args, **kwargs):
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html",{})
+    my_context = {
+        "my_text" : "This is about us",
+        "my_number": 123,
+        "my_list": [1,2,3,6,852,84,5874,878,54,8,"jjj"],
+    }
+    return render(request, "about.html",my_context)
 
 
 def social_view(request, *args, **kwargs):
